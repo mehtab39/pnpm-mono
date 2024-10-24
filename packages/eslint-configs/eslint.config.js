86ecffb6b50c9158d2de9b28c331a3b543abee89
mehtab39 <mehtabgill122@gmail.com>
2024-10-24 10:30:05 +0530

    fix: handle eslint

--- packages/eslint-configs/eslint.config.js
+++ packages/eslint-configs/eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -18,7 +18,7 @@ export default [
       },
     },
     settings: { react: { version: '18.3' } },
-    plugins: {
+    plugins: `{
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
